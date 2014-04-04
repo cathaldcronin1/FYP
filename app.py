0c4489a94fd7fba4bb09fda9fc0a42a6aa4ee6b9
cathaldcronin1 <cathaldcronin1@yahoo.co.uk>
2014-04-04 20:50:37 +0100

    changed monog db URL for heroku

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from python.details import Details
 app = flask.Flask(__name__)
 
 # Establish connection to MongoDB
-connection = MongoClient(os.environ['MONGOHQ_URL'])
+connection = MongoClient("mongodb://cathal:Zelda#0018@oceanic.mongohq.com:10032/app23744423")
 
 # Perform First time setup
 setup = Setup(connection)
